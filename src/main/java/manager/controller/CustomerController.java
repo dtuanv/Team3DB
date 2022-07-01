2a5c9488f983b3274283aa21d3e91b43668226aa
dtuan <dtuanlieb@gmail.com>
2022-07-01 17:49:26 +0200

    Notification when saving new data

--- src/main/java/manager/controller/CustomerController.java
+++ src/main/java/manager/controller/CustomerController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -24,9 +21,15 @@ public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
     @RequestMapping(value={"/customer"})
-    public String displayDatabase(Customer customer, Model model){
+    public String displayDatabase(Customer customer, Model model, @RequestParam String check){
         model.addAttribute("customer",new Customer());
         model.addAttribute("delivery", new Delivery());
+
+        String message = null;
+        if(check != null){
+            message = "Thank you!!!";
+        }
+        model.addAttribute("errorMessage",message);
 
         return "customer.html";
     }
@@ -35,17 +38,16 @@ public class CustomerController {
     @PostMapping("/saveCustomer")
     public String saveCustomer(Model model,
                                @Valid @ModelAttribute("delivery") Delivery delivery,
-                                 Errors errors,
-                                 @Valid @ModelAttribute("customer") Customer customer
+                               Errors errors
+                               ,@Valid @ModelAttribute("customer") Customer customer
                                ,Errors error
             ) {
-
-
         if(error.hasErrors() || errors.hasErrors()){
             return "customer.html";
         }
+
         customerService.isSavedCustomer(customer,delivery);
-        return "redirect:/customer";
+        return "redirect:/customer?check=1";
     }
     @GetMapping("/manager")
     public String showCustomer(Model model){
