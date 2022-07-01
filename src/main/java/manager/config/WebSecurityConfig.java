79e14afa9bb9c31b49173c768a524e5729040269
dtuan <dtuanlieb@gmail.com>
2022-07-01 12:41:13 +0200

    Bug fixed

--- src/main/java/manager/config/WebSecurityConfig.java
+++ src/main/java/manager/config/WebSecurityConfig.java
@@ -1,14 +1,40 @@
 /*package manager.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public void configure(HttpSecurity httpSecurity){
-        httpSecurity.csrf().disable().
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/manager").permitAll()
+                .mvcMatchers("customer").permitAll()
+                .and()
+                .formLogin()
+
+                .and().httpBasic();
+
+    }
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("dtuan").password("54321").roles("ADMIN")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
 
+
+
  */
+
