ce03fb7c8d1e29ffe44946bdc93fc2ef006f9263
dtuan <dtuanlieb@gmail.com>
2022-07-04 21:56:52 +0200

    Order Page with filter

--- src/main/java/manager/controller/OrderController.java
+++ src/main/java/manager/controller/OrderController.java
@@ -8,7 +8,9 @@ import manager.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,17 +23,61 @@ public class OrderController {
    private ProductRepository productRepository;
 
     @RequestMapping("/order")
-    public String displayOrder(Model model){
+    public String displayOrder(Model model, @RequestParam(name="check",required = false)int check){
      /*   List<DietType> dietTypeList = dietTypeRepository.getDietName();
        List<DietType> dietTypeList = orderRepository.getDietName();
         model.addAttribute("type",dietTypeList);
 
 
       */
+        System.out.println("check:::"+check);
+        if(check==1){
+        //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Calorie");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
+        if(check==2){
+            //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Vegetarisch");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
+        if(check==3){
+            //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Fat");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
+        if(check==4){
+            //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Glutenfrei");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
+        if(check==5){
+            //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Flutarian");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
+        if(check==6){
+            //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Hight");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
+        if(check==7){
+            //    System.out.println("checkin:::"+check);
+            List<Product> product = productRepository.getAllProductWithCal("Fleisch");
+            model.addAttribute("product",product);
+            return "orders.html";
+        }
         List<Product> product = productRepository.getAllProduct();
         model.addAttribute("product",product);
         return "orders.html";
     }
+
 
 
 }
--- src/main/java/manager/repository/ProductRepository.java
+++ src/main/java/manager/repository/ProductRepository.java
@@ -13,4 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query(value = "SELECT * FROM product Inner JOin product_diet_type USING (product_id)" +
             " INNER JOIN diet_type USING(diet_type_id)" , nativeQuery = true)
     public List<Product> getAllProduct();
+    @Query(value = "SELECT * FROM product Inner JOin product_diet_type USING (product_id)" +
+            " INNER JOIN diet_type USING(diet_type_id) WHERE diet_type_name LIKE %?1% " , nativeQuery = true)
+    public List<Product> getAllProductWithCal(String name);
 }
